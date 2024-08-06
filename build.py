f9ded7aa868d69660f75673ecf56799d3e3e0ad5
00glitched <conaeze3@gmail.com>
2024-08-06 11:56:20 -0300

    wav to c

--- build.py
+++ build.py
@@ -1,16 +1,20 @@
 import os
+from res.tileset.maketileset import IMAGE_TO_C
+from res.sample.makesample import WAV_TO_C
+
+
 myPath = os.path.dirname(os.path.abspath(__file__))
 
 #for tileset
 tileset_list = ["test"]
-exec (open (myPath+"/res/tileset/maketileset.py").read())
+#exec (open (myPath+"/res/tileset/maketileset.py").read())
 IMAGE_TO_C(myPath+"/res/tileset",tileset_list)
 
 #for tilemap
 
 #for samples
-#sample_list = ["test"]
+sample_list = ["test"]
 #exec (open (myPath+"/res/tileset/makesample.py").read())
-#WAV_TO_C(myPath+"/res/tileset",tileset_list)
+WAV_TO_C(myPath+"/res/sample",sample_list)
 
 #for presets
--- res/sample/makesample.py
+++ res/sample/makesample.py
@@ -1,7 +1,60 @@
-import wave as wv
+import wave as wav
+import struct
 
-# first: open wav file
-
-# sec: write an array
-
-# third: write c and wav file
+def WAV_TO_C(path,list):
+        for i in list:
+            print("Converting "+i+".wav into "+i+".c")
+            sound = wav.open(path+"/wav/"+i+".wav")
+            cfilePath = path+"/c/"+i+".c"
+            #get data
+            meta = sound.getparams()
+            frames_rate = meta.framerate
+            frames_num = meta.nframes
+            frames_width = meta.sampwidth
+            frames = sound.readframes(frames_rate)
+            
+            #converting
+            pcm_format = "<"+"h"*(len(frames)//frames_width)
+            pcm = struct.unpack(pcm_format,frames)
+            pcm_len = len(pcm)
+            
+            #export
+            framearr = []
+            framestr = "\nconst uint8_t "+i+"_sample[] =\n{\n"
+            if (pcm_len > 2**11):
+                kmod = int(2+pcm_len/(2**12))
+            else:
+                kmod = 1
+            t=0
+            for k in range(pcm_len):
+                if (k%kmod == 0):
+                    framestr += str(int(8+pcm[k]/4096))+","
+                    
+                    #byteA = int(pcm[k]+2**15)/(2**8)
+                    byteA = int(8+pcm[k]/4096)*16
+                    #print(byteB)
+                    #byteB = int((pcm[k]+2**15)%(2**8))
+                    #framearr.append(int(8+pcm[k]/4096)*2**10)
+                    framearr.append(byteA)
+                    #framearr.append(byteB)
+                    t=t+1
+            framestr_Len = len(framestr)
+            framestr = framestr[0:framestr_Len-1]
+            framestr += "\n};"+"\nconst uint8_t "+i+"_sample_Len = "+str(framestr_Len-1)+";\n"
+            
+            sound.close()
+            
+            WAVout = wav.open(path+"/c/"+i+"_out.wav", mode="wb")
+            WAVout.setnchannels(1)
+            WAVout.setsampwidth(1)
+            WAVout.setframerate(frames_rate/kmod)
+            WAVout.writeframes(bytearray(framearr))
+            WAVout.close()
+            
+            WAVtoC = open (cfilePath, mode = "w")
+            WAVtoC.write(framestr)
+            WAVtoC.close()
+            #print(framearr)
+        print(" OK.")
+    
+    
