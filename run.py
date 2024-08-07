55d0ae39ea6f49e3e73712ebbf89f25a8072b05a
00glitched <conaeze3@gmail.com>
2024-08-07 18:06:58 -0300

    run gb file

--- run.py
+++ run.py
@@ -2,7 +2,8 @@ import os
 from pyboy import PyBoy
 
 myPath = os.path.dirname(os.path.abspath(__file__))
-gb = PyBoy(myPath+"/obj/GBDJ.gb")
+
+gb = PyBoy(myPath+"/obj/GBDJ.gb", window="SDL2",scale=4)
 
 while gb.tick():
     pass
